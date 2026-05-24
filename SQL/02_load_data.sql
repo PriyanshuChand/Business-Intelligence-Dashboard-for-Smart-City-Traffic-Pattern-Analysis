91f3478ea0db72b1a64c341479c61ad787149c89
PriyanshuChand <priyanshu.pchand@gmail.com>
2026-05-24 12:21:32 +0530

    some changes

--- SQL/02_load_data.sql
+++ SQL/02_load_data.sql
@@ -1,9 +1,9 @@
-COPY traffic_trips
-FROM 'C:/traffic_project/delhi_traffic_cleaned.csv'
-DELIMITER ','
-CSV HEADER;
+-- COPY traffic_trips
+-- FROM 'C:/traffic_project/delhi_traffic_cleaned.csv'
+-- DELIMITER ','
+-- CSV HEADER;
 
-SELECT COUNT(*) FROM traffic_trips;
+-- SELECT COUNT(*) FROM traffic_trips;
 
 SELECT * FROM traffic_trips LIMIT 10
 
